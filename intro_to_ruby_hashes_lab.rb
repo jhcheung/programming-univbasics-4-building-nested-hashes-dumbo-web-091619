c01082fa293a5dcc95b7f4a3a749f7b6d64b5174
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-13 16:00:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,11 +19,14 @@ def monopoly_with_third_tier
       rent_in_dollars: {
         one_piece_owned: 25,
         two_pieces_owned: 50,
-        three_pieces_owned: 75,
-        four_pieces_owned: 100
+        three_pieces_owned: 100,
+        four_pieces_owned: 200
       },
       names: {
-        
+        name1: 20
+        name2: 4
+        name3: 23423
+        name4: 4324
       }
     }
   }
